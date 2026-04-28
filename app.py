2d4fbed48c930e294c041b74de6ecc7e35c46cef
272adeeb <120822950+272adeeb@users.noreply.github.com>
2026-04-28 00:46:24 -0700

    Refactor download function and update format map

--- app.py
+++ app.py
@@ -1,40 +1,16 @@
-from flask import Flask, request, jsonify, send_file, send_from_directory, make_response
-from flask_cors import CORS
+from flask import Flask, request, jsonify, send_file, send_from_directory
 import yt_dlp, os
 
 app = Flask(__name__)
-CORS(app, origins="*", supports_credentials=False)
-
 DOWNLOAD_FOLDER = 'downloads'
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
-
-FORMAT_MAP = {
-    'best':  'best[ext=mp4]/best',
-    '720p':  'best[height<=720][ext=mp4]/best[height<=720]',
-    '480p':  'best[height<=480][ext=mp4]/best[height<=480]',
-    'audio': 'bestaudio/best',
-}
-
-@app.after_request
-def add_cors(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    return response
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
 
-@app.route('/privacy')
-def privacy():
-    return send_from_directory('.', 'privacy.html')
-
-@app.route('/download', methods=['POST', 'OPTIONS'])
+@app.route('/download', methods=['POST'])
 def download():
-    if request.method == 'OPTIONS':
-        return make_response('', 204)
-
     data = request.get_json()
     url = data.get('url', '').strip()
     quality = data.get('quality', 'best')
@@ -42,20 +18,48 @@ def download():
     if not url:
         return jsonify({'error': 'Please enter a URL'}), 400
 
-    # Block YouTube URLs
-    if 'youtube.com' in url or 'youtu.be' in url:
-        return jsonify({'error': 'YouTube not supported. Try TikTok, Instagram, Twitter or Facebook.'}), 400
+    format_map = {
+        'best':  'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        '720p':  'bestvideo[height<=720][ext=mp4]+bestaudio/best[height<=720]',
+        '480p':  'bestvideo[height<=480][ext=mp4]+bestaudio/best[height<=480]',
+        'audio': 'bestaudio/best',
+    }
 
     ydl_opts = {
         'outtmpl': f'{DOWNLOAD_FOLDER}/%(title)s.%(ext)s',
-        'format': FORMAT_MAP.get(quality, FORMAT_MAP['best']),
+        'format': format_map.get(quality, format_map['best']),
         'noplaylist': True,
         'quiet': True,
         'nocheckcertificate': True,
-        'legacy_server_connect': True,
-        'retries': 10,
-        'fragment_retries': 10,
+        ydl_opts = {
+    'outtmpl': f'{DOWNLOAD_FOLDER}/%(title)s.%(ext)s',
+    'format': format_map.get(quality, format_map['best']),
+    'noplaylist': True,
+    'quiet': True,
+    'nocheckcertificate': True,
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['android', 'web'],
+        }
+    },
+    'legacy_server_connect': True,
+    'retries': 10,
+    'fragment_retries': 10,
+    'retry_sleep_functions': {'http': lambda n: 3},
+}
+
+'legacy_server_connect': True,
+'retries': 10,
+'fragment_retries': 10,
+'retry_sleep_functions': {'http': lambda n: 3},
     }
+
+    if quality == 'audio':
+        ydl_opts['postprocessors'] = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -78,4 +82,4 @@ def serve_file(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+app.run(host='0.0.0.0', port=port, debug=False)
